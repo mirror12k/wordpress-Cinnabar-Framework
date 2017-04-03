9a6b1daa065d000d497f9ceccdeebec94c5a9e13
mirror <mirror12k@gmail.com>
2017-04-02 23:36:28 -0400

    - implemented a proof-of-concept hiarchical custom-post-type mimicking the way wordpress pages work

---
+++ test-plugin/test-plugin.php
@@ -0,0 +1,92 @@
+<?php
+/*
+  Plugin Name: Test Plugin
+  Plugin URI:
+  Description: ?
+  Author: mirror12k
+  Version: 0.0.1
+  Author URI: http://www.www.www/
+*/
+
+if (!defined('ABSPATH')) die('indirect access');
+
+
+
+class TestPlugin
+{
+	public static function load_plugin()
+	{
+		add_action('init', array('TestPlugin', 'wordpress_init'));
+		add_filter('post_type_link', array('TestPlugin', 'rewrite_test_post_url'), 10, 3);
+		add_action('pre_get_posts', array('TestPlugin', 'add_test_post_to_pages'));
+
+	}
+
+	public static function wordpress_init()
+	{
+		error_log('Test Plugin wordpress_init');
+
+		TestPlugin::register_test_post_type();
+	}
+
+	public static function register_test_post_type()
+	{
+		register_post_type('test_plugin_post', array(
+			'labels' => array(
+				'name' => __( 'Test Posts', 'test_plugin_post' ),
+				'singular_name' => __( 'Test Post', 'test_plugin_post' ),
+				'add_new' => __( 'Add New', 'test_plugin_post' ),
+				'add_new_item' => __( 'Add New Test Post', 'test_plugin_post' ),
+				'edit_item' => __( 'Edit Test Posts', 'test_plugin_post' ),
+				'new_item' => __( 'New Test Post', 'test_plugin_post' ),
+				'view_item' => __( 'View Test Post', 'test_plugin_post' ),
+				'search_items' => __( 'Search Test Posts', 'test_plugin_post' ),
+				'not_found' => __( 'No Test Posts found', 'test_plugin_post' ),
+				'not_found_in_trash' => __( 'No Test Posts found in Trash', 'test_plugin_post' ),
+				'parent_item_colon' => __( 'Parent Test Post:', 'test_plugin_post'),
+				'menu_name' => __( 'Test Posts', 'test_plugin_post' ),
+			),
+			'hierarchical' => true,
+			'description' => __( 'Test Posts', 'test_plugin_post' ),
+			'supports' => array( 'title', 'editor', 'comments', 'page-attributes' ),
+			'public' => true,
+			'show_ui' => true,
+			'show_in_menu' => true,
+			// 'show_in_nav_menus' => true,
+			'publicly_queryable' => true,
+			'exclude_from_search' => true,
+			'has_archive' => true,
+			'query_var' => true,
+			'can_export' => true,
+			'rewrite' => array('slug' => false, 'with_front' => false),
+			'capability_type' => 'post'
+		));
+	}
+
+	// taken and modified from https://wordpress.stackexchange.com/questions/203951/remove-slug-from-custom-post-type-post-urls
+	function rewrite_test_post_url($post_link, $post, $leavename) {
+		if ('test_plugin_post' != $post->post_type || 'publish' != $post->post_status)
+			return $post_link;
+
+		$post_link = str_replace( '/' . $post->post_type . '/', '/', $post_link );
+
+		return $post_link;
+	}
+
+	// taken and modified from https://wordpress.stackexchange.com/questions/203951/remove-slug-from-custom-post-type-post-urls
+	function add_test_post_to_pages($query) {
+		// || 2 != count($query->query) || !isset($query->query['page'])
+		if (!$query->is_main_query())
+			return;
+
+		// if (!empty( $query->query['name']))
+			$query->set('post_type', array('post', 'test_plugin_post', 'page'));
+	}
+
+
+
+
+TestPlugin::load_plugin();
+
+
+
