e74be68e26f8f0819a9879360113e028b2b88c9b
mirror <mirror12k@gmail.com>
2019-10-25 16:52:34 -0400

    - implemented a way to skip wp header/footer on synth pages

--- src/Cinnabar/Mixin/SyntheticPageManager/twig-template.php
+++ src/Cinnabar/Mixin/SyntheticPageManager/twig-template.php
@@ -1,19 +1,30 @@
-<?php if (!defined('ABSPATH')) die('indirect access'); ?>
 <?php
-get_header();
+// security
+if (!defined('ABSPATH')) die('indirect access');
 
+// access the manager to get our page details
 global $synthetic_manager;
 
 
 
+// render the arguments to the template
 $template_args = array( 'app' => $synthetic_manager->app );
 if (isset($synthetic_manager->active_view_controller))
 	$template_args = array_merge($template_args, $synthetic_manager->active_view_controller->template_args());
 
-// foreach ($template_args as $key => $val)
-// 	error_log("debug template args: $key => " . json_encode($val));
+
+
+// render header unless disabled
+if (!isset($synthetic_manager->active_synthetic_page['skip_header_footer']))
+	get_header();
+
+
+
+// render the template
 echo $synthetic_manager->render_template($synthetic_manager->active_synthetic_page['template'], $template_args);
 
 
 
-get_footer();
+// render footer unless disabled
+if (!isset($synthetic_manager->active_synthetic_page['skip_header_footer']))
+	get_footer();
